778791e774f56eb994b3915d5ed541b6d5b75ce9
Gavin John <gavinnjohn@gmail.com>
2026-07-29 14:54:55 -0400

    Initial Commit

---
+++ PandaAlarm/PandaAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaAlarmApp.swift
+//  PandaAlarm
+//
+//  Created by Gavin John on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PandaAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
